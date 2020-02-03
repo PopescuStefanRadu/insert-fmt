e39fd5fd1f5689e6baa3e4db262b593eca8d1c44
ArxFatalisFan <noone@example.com>
2020-02-03 02:29:58 +0200

    Inserts by columns tested

--- fun-area/go-fun.go
+++ fun-area/go-fun.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/xwb1989/sqlparser"
 	"io"
 	"os"
@@ -21,23 +22,23 @@ func main() {
 	insertsByTable := getInsertsByTable(inserts)
 
 	insertsByColumns := make(map[string][]*sqlparser.Insert)
-	for _, inserts := range insertsByTable {
-		if len(inserts) > 1 {
-			for _, sameTable := range inserts {
-				var orderedColumns = make(sqlparser.Columns, len(sameTable.Columns))
-				copy(sameTable.Columns, orderedColumns)
-				sort.Slice(orderedColumns, func(i, j int) bool {
-					return orderedColumns[i].CompliantName() < orderedColumns[j].CompliantName()
-				})
-				colNames := make([]string, len(sameTable.Columns))
-				for _, column := range orderedColumns {
-					colNames = append(colNames, column.Lowered())
-				}
-				columnsAsString := strings.Join(colNames, ",")
-				insertsByColumns[columnsAsString] = append(insertsByColumns[columnsAsString], sameTable)
+	for _, inserts := range *insertsByTable {
+		for _, sameTableInsert := range inserts {
+			columns := sameTableInsert.Columns
+			var orderedColumns = make(sqlparser.Columns, len(columns))
+			copy(orderedColumns, columns)
+			sort.Slice(orderedColumns, func(i, j int) bool {
+				return orderedColumns[i].CompliantName() < orderedColumns[j].CompliantName()
+			})
+			colNames := make([]string, len(columns))
+			for i, column := range orderedColumns {
+				colNames[i] = column.Lowered()
 			}
+			columnsAsString := strings.Join(colNames, ",")
+			insertsByColumns[columnsAsString] = append(insertsByColumns[columnsAsString], sameTableInsert)
 		}
 	}
+	fmt.Println(insertsByColumns)
 }
 
 func getInserts(reader io.Reader) []*sqlparser.Insert {
@@ -52,21 +53,16 @@ func getInserts(reader io.Reader) []*sqlparser.Insert {
 		if insert, ok := statement.(*sqlparser.Insert); ok {
 			statements = append(statements, insert)
 		} else {
-			_, _ = os.Stderr.WriteString("Non-insert statement present")
+			_, _ = os.Stderr.WriteString("[Warning] - Non-insert statement present\r\n")
 		}
 	}
 	return statements
 }
 
-func getInsertsByTable(inserts []*sqlparser.Insert) map[string][]*sqlparser.Insert {
+func getInsertsByTable(inserts []*sqlparser.Insert) *map[string][]*sqlparser.Insert {
 	var insertsByTable = make(map[string][]*sqlparser.Insert)
 	for _, insert := range inserts {
 		insertsByTable[insert.Table.Name.String()] = append(insertsByTable[insert.Table.Name.String()], insert)
 	}
-	return insertsByTable
+	return &insertsByTable
 }
-
-//if insert, ok := statement.(*sqlparser.Insert); ok {
-//	statements = append(statements, insert)
-//} else {
-//}
